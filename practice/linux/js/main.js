d2193e28b911e30567a896c12eb9efa661f76970
cyberblack28 <cyberblack2006@gmail.com>
2020-02-11 17:02:21 +0900

    20200211 vue.js repair

---
+++ practice/linux/js/main.js
@@ -0,0 +1,21 @@
+Vue.component('header-title',{
+    template : '<header class="header_area"><h1>Linux 演習課題</h1></header>'
+})
+
+new Vue({
+    el: '#header',
+    data: {
+        
+    }
+})
+
+Vue.component('footer-content',{
+    template : '<footer class="footer_area"><p class="text_align_center">&copy; cyberblack28</p></footer>'
+})
+
+new Vue({
+    el: '#footer',
+    data: {
+        
+    }
+})
