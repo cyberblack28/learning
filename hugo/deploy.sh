702337ea12c1d542a40b416a7c49e618c1d5a3d8
cyberblack28 <cyberblack2006@gmail.com>
2020-04-06 18:56:38 +0900

    20200406 hugo test

---
+++ hugo/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+#   -t: set theme
+#   --minift: compaction
+#   --enableGitInfo: use commit timestamp as lastmod
+hugo -t hugo-theme-techdoc --minify --enableGitInfo
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
